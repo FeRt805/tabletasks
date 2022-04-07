17789b30057affe31eab2f56f134908eb1c4fcab
Alex <orando20056@ya.ru>
2022-04-07 07:52:01 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,93 @@
+'use strict'
+
+// const switcher = document.querySelector('.btn');
+
+// switcher.addEventListener('click', function() {
+//     console.log('current class name: ' );
+// });
+// document.querySelector("select").addEventListener("change", function(e){
+let selects = document.querySelectorAll("select");
+// alert(selects[0].length);
+document.getElementById("apre").innerHTML = localStorage.getItem("team1_a_sum");
+document.getElementById("bpre").innerHTML = localStorage.getItem("team1_b_sum");
+document.getElementById("cpre").innerHTML = localStorage.getItem("team1_c_sum");
+document.getElementById("dpre").innerHTML = localStorage.getItem("team1_d_sum");
+localStorage["team1_sum"] = Number(localStorage["team1_a_sum"]) + Number(localStorage["team1_b_sum"] + Number(localStorage["team1_c_sum"]) + Number(localStorage["team1_d_sum"]));
+document.getElementById("everything_sum").innerHTML = localStorage["team1_sum"];
+for (let k=0; k < selects.length; k++){
+    
+    // document.querySelector("select").addEventListener("change", function(e){
+    // let val = e.target.value;
+    selects[k].addEventListener("change", function(e){
+    // let val = selects[k].value;
+    let val = e.target.value;
+    // alert(val);
+    // localStorage.clear()
+    if (val[0] == "a"){
+        var sum = localStorage.getItem("team1_a_sum");
+        var bred = localStorage.getItem("t_sadsdasum");
+        if (sum == bred){
+            localStorage["team1_a_sum"] = Number(val.substr(1));
+        }
+        else{
+            localStorage["team1_a_sum"] = Number(sum) + Number(val.substr(1));
+        }
+        // localStorage.setItem("team1_a_sum", Number(sum) + Number(val.substr(1)));
+        // alert(localStorage.getItem("team1_a_sum"));
+        
+        document.getElementById("apre").innerHTML = localStorage.getItem("team1_a_sum");
+        document.getElementById("everything_sum").innerHTML = Number(localStorage["team1_a_sum"]) + Number(localStorage["team1_b_sum"] + Number(localStorage["team1_c_sum"]) + Number(localStorage["team1_d_sum"]));
+        document.getElementById("everything_sum").innerHTML = localStorage["team1_sum"];
+    }
+    if (val[0] == "b"){
+        var sum = localStorage.getItem("team1_b_sum");
+        var bred = localStorage.getItem("t_sadsdasum");
+        if (sum == bred){
+            localStorage["team1_b_sum"] = Number(val.substr(1));
+        }
+        else{
+            localStorage["team1_b_sum"] = Number(sum) + Number(val.substr(1));
+        }
+        // localStorage.setItem("team1_a_sum", Number(sum) + Number(val.substr(1)));
+        // alert(localStorage.getItem("team1_b_sum"));
+        document.getElementById("bpre").innerHTML = localStorage.getItem("team1_b_sum");
+        document.getElementById("everything_sum").innerHTML = Number(localStorage["team1_a_sum"]) + Number(localStorage["team1_b_sum"] + Number(localStorage["team1_c_sum"]) + Number(localStorage["team1_d_sum"]));
+        document.getElementById("everything_sum").innerHTML = localStorage["team1_sum"];
+    }
+    if (val[0] == "c"){
+        var sum = localStorage.getItem("team1_c_sum");
+        var bred = localStorage.getItem("t_sadsdasum");
+        if (sum == bred){
+            localStorage["team1_c_sum"] = Number(val.substr(1));
+        }
+        else{
+            localStorage["team1_c_sum"] = Number(sum) + Number(val.substr(1));
+        }
+        // localStorage.setItem("team1_a_sum", Number(sum) + Number(val.substr(1)));
+        // alert(localStorage.getItem("team1_c_sum"));
+        document.getElementById("cpre").innerHTML = localStorage.getItem("team1_c_sum");
+        document.getElementById("everything_sum").innerHTML = Number(localStorage["team1_a_sum"]) + Number(localStorage["team1_b_sum"] + Number(localStorage["team1_c_sum"]) + Number(localStorage["team1_d_sum"]));
+        document.getElementById("everything_sum").innerHTML = localStorage["team1_sum"];
+    }
+    if (val[0] == "d"){
+        var sum = localStorage.getItem("team1_d_sum");
+        var bred = localStorage.getItem("t_sadsdasum");
+        if (sum == bred){
+            localStorage["team1_d_sum"] = Number(val.substr(1));
+        }
+        else{
+            localStorage["team1_d_sum"] = Number(sum) + Number(val.substr(1));
+        }
+        // localStorage.setItem("team1_a_sum", Number(sum) + Number(val.substr(1)));
+        // alert(localStorage.getItem("team1_d_sum"));
+        document.getElementById("dpre").innerHTML = localStorage.getItem("team1_d_sum");
+        document.getElementById("everything_sum").innerHTML = Number(localStorage["team1_a_sum"]) + Number(localStorage["team1_b_sum"] + Number(localStorage["team1_c_sum"]) + Number(localStorage["team1_d_sum"]));
+        document.getElementById("everything_sum").innerHTML = localStorage["team1_sum"];
+    }
+    localStorage["team1_sum"] = Number(localStorage["team1_a_sum"]) + Number(localStorage["team1_b_sum"] + Number(localStorage["team1_c_sum"]) + Number(localStorage["team1_d_sum"]));
+    document.getElementById("everything_sum").innerHTML = localStorage["team1_sum"];
+    }
+    )
+    
+
+}    
